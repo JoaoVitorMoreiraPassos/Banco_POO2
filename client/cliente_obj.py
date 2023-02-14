05ec1b163f1b4d053e5df841447d0d69e677253f
João Vitor Moreira Passos <moreirapassosj@gmail.com>
2023-02-14 10:11:10 -0300

    up

--- client/cliente_obj.py
+++ client/cliente_obj.py
@@ -1,5 +1,31 @@
 class Cliente:
+    """
+    A classe representa um cliente do banco.
 
+    ...
+
+    Attributes
+    ----------
+    _id : int
+        id do cliente
+    _nome : str
+        nome do cliente
+    _cpf : str
+        cpf do cliente
+    _nascimento : str
+        data de nascimento do cliente
+    _email : str
+        email do cliente
+    _contas : dict
+        dicionário com as contas do cliente
+
+    Methods
+    -------
+    add_cc(conta):
+        adiciona uma conta corrente ao cliente
+    add_cp(conta):
+        adiciona uma conta poupança ao cliente
+    """
     __slots__ = ["_id", "_nome", "_cpf", "_nascimento", "_email", "_contas"]
 
     def __init__(self, id, nome, cpf, nascimento, email):
@@ -12,6 +38,7 @@ class Cliente:
 
     def __str__(self):
         return f"id: {self._id} | Nome: {self._nome} | CPF: {self._cpf} | Nascimento: {self._nascimento} | Email: {self._email} | Contas: {self._contas}"
+
     def add_cc(self, conta):
         self._contas["cc"] = conta
 
